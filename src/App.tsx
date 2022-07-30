fed2fa57134608deab734e9749ec83f2395a7f51
Aaron Hooper <aaronhoopermail@gmail.com>
2022-07-31 00:33:16 +1200

    Display api in formatted table

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,10 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 type Cocktail = {
+  [key: string]: string;
   strDrink: string;
   strAlcoholic: string;
   strGlass: string;
@@ -54,7 +56,8 @@ function App() {
   const [cocktailData, setCocktailData] = useState<null | undefined | any>(undefined);
 
   const COCKTAIL_BASE_URL = "https://www.thecocktaildb.com/api/json/v1/";
-
+  const measures = ["strMeasure1", "strMeasure2", "strMeasure3", "strMeasure4", "strMeasure5", "strMeasure6", "strMeasure7", "strMeasure8", "strMeasure9", "strMeasure10", "strMeasure11", "strMeasure12", "strMeasure13", "strMeasure14", "strMeasure15"];
+  const ingredients = ["strIngredient1", "strIngredient2", "strIngredient3", "strIngredient4", "strIngredient5", "strIngredient6", "strIngredient7", "strIngredient8", "strIngredient9", "strIngredient10", "strIngredient11", "strIngredient12", "strIngredient13", "strIngredient14", "strIngredient15"]
   return (
     <div>
       <h1>Cocktail Recipe Search</h1>
@@ -69,7 +72,7 @@ function App() {
       
       {/**output display for cocktail data */}
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650}} aria-label="cocktail-recipes">
+        <Table sx={{ minWidth: 650 }} aria-label="cocktail-recipes">
           <TableHead>
               {cocktailData === undefined ? (
                 <TableRow><TableCell></TableCell></TableRow>
@@ -77,20 +80,58 @@ function App() {
                 <TableRow>{cocktailData === null ? (
                   <TableCell>Cocktail not found</TableCell>
                 ) : (
-                  <TableCell align="center" colSpan={2}>{cocktailData.drinks[0].strDrink} Recipes</TableCell>
+                  <TableCell align="center" colSpan={2}>{cocktailData.drinks[0].strDrink} Recipe(s)</TableCell>
                 )}</TableRow>
               )}
           </TableHead>
-          <TableBody>
-            {cocktailData.drinks.map((drink: Cocktail) => (
-              <TableRow key={drink.strDrink}>
-                <TableCell>{drink.strDrink}</TableCell>
-                <TableCell>Type: {drink.strAlcoholic}</TableCell>
-                <TableCell>Preferred glass: {drink.strGlass}</TableCell>
-                <TableCell><img src={drink.strDrinkThumb}/></TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
+          {cocktailData === undefined || cocktailData === null ? (
+            <TableBody></TableBody>
+          ) : (
+            <TableBody>
+              {cocktailData.drinks.map((drink: Cocktail) => (
+                <Box key={drink.strDrink}>
+                  <Table>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell colSpan={3} align="center" sx={{fontWeight: "bold"}}>{drink.strDrink}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell rowSpan={19} sx={{width: 400}}><img src={drink.strDrinkThumb} style={{maxWidth: "100%", height: "auto"}}/></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell>Type: {drink.strAlcoholic}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell></TableCell>
+                        <TableCell>Preferred glass: {drink.strGlass}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell></TableCell>
+                        <TableCell sx={{fontWeight: "bold"}}>Ingredients</TableCell>
+                      </TableRow>
+                      {measures.map((measure: string, index) => (
+                        drink[measure] === null ? (
+                          <TableRow key={measure}></TableRow>
+                        ): (
+                          <TableRow key={measure}>
+                            <TableCell></TableCell>
+                            <TableCell>{drink[measure]} {drink[ingredients[index]]}</TableCell>
+                          </TableRow>
+                        )
+                      ))}
+                      <TableRow>
+                        <TableCell colSpan={3}>{drink.strInstructions}</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table> 
+                </Box>
+              ))}
+            </TableBody>
+          )}
+          
         </Table>
       </TableContainer>
 
