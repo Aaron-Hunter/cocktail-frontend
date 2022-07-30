e023abadb2564c37e7d083fc040d01cd76f4448a
Aaron Hooper <aaronhoopermail@gmail.com>
2022-07-30 18:01:17 +1200

    Make api data output to basic table

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 import axios from "axios";
 import './App.css';
-
-console.log(process.env.REACT_APP_COCKTAIL_API_KEY);
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function App() {
   //Declare state variable for name of cocktail
   const [cocktailName, setCocktailName] = useState("");
+  const [cocktailData, setCocktailData] = useState<null | undefined | any>(undefined);
 
   const COCKTAIL_BASE_URL = "https://www.thecocktaildb.com/api/json/v1/";
 
@@ -21,19 +27,51 @@ function App() {
         <button onClick={search}>Search</button>
         <button onClick={random}>Random Cocktail</button>
       </div>
+      
+      {/**output display for cocktail data */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650}} aria-label="cocktail-recipes">
+          <TableHead>
+              {cocktailData === undefined ? (
+                <TableRow><TableCell></TableCell></TableRow>
+              ) : (
+                <TableRow>{cocktailData === null ? (
+                  <TableCell>Cocktail not found</TableCell>
+                ) : (
+                  <TableCell align="center" colSpan={2}>{cocktailData.drinks[0].strDrink} Recipes</TableCell>
+                )}</TableRow>
+              )}
+          </TableHead>
+          <TableBody>
+            {cocktailData.drinks.map((drink: any) => (
+              <TableRow key={drink.strDrink}>
+                <TableCell>{drink.strDrink}</TableCell>
+                <TableCell>Type: {drink.strAlcoholic}</TableCell>
+                <TableCell>Preferred glass: {drink.strGlass}</TableCell>
+                <TableCell><img src={drink.strDrinkThumb}/></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      
     </div>
   );
 
   function search() {
     axios.get(COCKTAIL_BASE_URL + process.env.REACT_APP_COCKTAIL_API_KEY + "/search.php?s=" + cocktailName).then((res) => {
-      console.log(res.data);
-    })
+      setCocktailData(res.data);
+    });
+    
   }
 
   function random() {
     axios.get(COCKTAIL_BASE_URL + process.env.REACT_APP_COCKTAIL_API_KEY + "/random.php").then((res) => {
-      console.log(res.data);
-    })
+      setCocktailData(res.data);
+      
+    });
+    
   }
 }
 
